e5cab520081050b8f7ebea124b700033788b57bf
ralef555 <ralef8078@gmail.com>
2022-05-11 20:48:16 -0400

    Initial commit

---
+++ app/src/main/java/com/example/sorteiopremio/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.sorteiopremio;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
